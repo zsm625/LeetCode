3ed325a4bf7470edf1259295b30f55a9a11f145c
zsm625 <zhoushimin_625@163.com>
2020-06-19 17:49:57 +0800

    判断字符是否为回文，添加以字符串作为参数的方法

--- src/office66/IsPalindrome.java
+++ src/office66/IsPalindrome.java
@@ -39,9 +39,51 @@ public class IsPalindrome {
 		}
 		
 	}
+	
+	/**
+	 * 给定一个字符串，验证它是否是回文串，
+	 * 只考虑字母和数字字符，可以忽略字母的大小写。
+	 * 
+	 * 在原字符串上进行操作，采用双指针的方式，head指针从前边慢慢遍历，trail指针从最后开始遍历
+	 * 直到head>=trail
+	 * 其中，在原字符串上遍历需要注意的是如果遇到不是字符或者数字的就需要跳过，进入下一个字符
+	 * 采用了Character类中的方法来判断
+	 *@author ZSM
+	 * @param s
+	 * @return
+	 */
+	public boolean isPalindrome(String s) {
+		if(s==null) {
+			return false;
+		}
+		char[] chars = s.toCharArray();
+		int head=0,trail = s.length()-1;
+		while(head<=trail) {
+			while(!Character.isLetterOrDigit(chars[head])) {
+				if(head>=chars.length-1) {
+					break;
+				}
+				head++;
+			}
+			while(!Character.isLetterOrDigit(chars[trail])) {
+				if(trail<=0) {
+					break;
+				}
+				trail--;
+			}
+			if((head<=trail)&&Character.toLowerCase(chars[head])!=Character.toLowerCase(chars[trail])) {
+				return false;
+			}
+			head++;
+			trail--;
+		}
+		return true;
+	}
 	public static void main(String[] args) {
 		IsPalindrome palindrome = new IsPalindrome();
-		boolean b = palindrome.isPalindrome(313);
-		System.out.println(b);
+//		boolean b = palindrome.isPalindrome(313);
+//		System.out.println(b);
+		boolean c = palindrome.isPalindrome(".,");
+		System.out.println(c);
 	}
 }
