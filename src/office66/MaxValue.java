dd663eda96680ef7ba548d43986b71d6b0be9441
zsm625 <zhoushimin_625@163.com>
2020-07-07 16:44:11 +0800

    礼物的最大价值

---
+++ src/office66/MaxValue.java
@@ -0,0 +1,36 @@
+package office66;
+
+/**
+ * 礼物的最大价值
+ * 在一个 m*n 的棋盘的每一格都放有一个礼物，
+ * 每个礼物都有一定的价值（价值大于 0）。
+ * 你可以从棋盘的左上角开始拿格子里的礼物，
+ * 并每次向右或者向下移动一格、直到到达棋盘的右下角。
+ * 给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+ * @author ZSM
+ *
+ * @time 2020年7月7日上午11:42:22
+ */
+public class MaxValue {
+	/**
+	 * 动态规划
+	 *@author ZSM
+	 * @param grid
+	 * @return
+	 */
+	public int maxValue(int[][] grid) {
+		if(grid==null || grid.length==0 || grid[0].length==0) {
+			return 0;
+		}
+		int m = grid.length;
+		int n = grid[0].length;
+		int[][] dp = new int[m+1][n+1];
+		for(int i=1;i<=m;i++) {
+			for(int j=1;j<=n;j++) {
+				dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])+grid[i-1][j-1];
+			}
+		}
+		return dp[m][n];
+	}
+
+}
