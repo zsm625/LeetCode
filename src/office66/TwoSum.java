588a49589c355b70450e4b987b87e411a6e3a51f
zsm625 <zhoushimin_625@163.com>
2020-07-20 15:10:40 +0800

    两数之和II，返回目标数之和的下标

--- src/office66/TwoSum.java
+++ src/office66/TwoSum.java
@@ -1,6 +1,9 @@
 package office66;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -48,14 +51,79 @@ public class TwoSum {
 			}
 			map.put(nums[i], num);
 		}
-		
 		return res;
-		
 	}
+	
+	/**
+	 * 两数之和II
+	 * 给定一个已按照升序排列的有序数组，
+	 * 找到两个数使得它们相加之和等于目标数。
+	 * 函数应该返回这两个下标值 index1 和 index2，
+	 * 其中 index1 必须小于 index2
+	 * 
+	 * 第一种方法：使用hash表来解决，但是超出时间限制
+	 *@author ZSM
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSum3(int[] nums ,int target) {
+		if(nums==null || nums.length==0) {
+			return nums;
+		}
+		List<int[]> asList = Arrays.asList(nums);
+		List<Integer> list = new ArrayList<>(nums.length);
+		for(int i=0;i<nums.length;i++) {
+			list.add(nums[i]);
+		}
+		int[] res = new int[2];
+		for(int j=0;j<nums.length;j++) {
+			int temp = target-nums[j];
+			if(list.contains(temp)) {
+				if(j!=list.lastIndexOf(temp)) {
+					res[0] = j+1;
+					res[1] =1+(list.lastIndexOf(temp));
+					break;
+				}
+			}
+		}
+		return res;
+	}
+	
+	/**
+	 * 双指针法
+	 *@author ZSM
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSum4(int[] nums,int target) {
+		if(nums==null || nums.length==0) {
+			return new int[]{-1,-1};
+		}
+		int[] res = new int[2];
+		int left = 0,right = nums.length-1;
+		while(left<=right) {
+			int temp = nums[left]+nums[right];
+			if(temp==target) {
+				res[0] = left+1;
+				res[1] = right+1;
+				break;
+			}
+			if(temp<target) {
+				left++;
+			}
+			if(temp>target) {
+				right--;
+			}
+		}
+		return res;
+	}
+	
 	public static void main(String[] args) {
 		TwoSum sum = new TwoSum();
-		int[] nums = {2,7,11,15};
-		int[] is = sum.twoSum2(nums, 9);
+		int[] nums = {0,0,11,15};
+		int[] is = sum.twoSum4(nums, 0);
 		for(int i=0;i<2;i++) {
 			System.out.print(is[i]+"\t");
 		}
