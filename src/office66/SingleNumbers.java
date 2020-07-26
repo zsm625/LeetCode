933a8bf9cc4f1ec5393ceffb93f6f4f14c57d53c
zsm625 <zhoushimin_625@163.com>
2020-07-26 11:28:58 +0800

    数组中出现数字的次数II

--- src/office66/SingleNumbers.java
+++ src/office66/SingleNumbers.java
@@ -1,5 +1,8 @@
 package office66;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 找到数组中只出现过一次的序列
  * @author ZSM
@@ -32,5 +35,45 @@ public class SingleNumbers {
 			}
 		}
 		return new int[] {res,res^x};
+	}
+	
+	/**
+	 * 数组中出现数字的次数II
+	 * 在一个数组 nums 中除一个数字只出现一次之外，
+	 * 其他数字都出现了三次。请找出那个只出现一次的数字。
+	 *@author ZSM
+	 * @param nums
+	 * @return
+	 */
+	public int singleNumbers2(int[] nums) {
+		if(nums==null || nums.length==0) {
+			return -1;
+		}
+		//最先想到的方式是hash表来做
+		int res = -1;
+		Map<Integer,Integer> map = new HashMap<>();
+		for(int i=0;i<nums.length;i++) {
+			if(!map.containsKey(nums[i])) {
+				map.put(nums[i], 1);
+			}else {
+				Integer temp = map.get(nums[i]);
+				map.put(nums[i], temp+1);
+			}
+			
+		}
+		
+		for (int i : nums) {
+			if(map.get(i)==1) {
+				res = i;
+			}
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		SingleNumbers numbers = new SingleNumbers();
+		int[] nums= {9,1,7,9,7,9,7};
+		int i = numbers.singleNumbers2(nums);
+		System.out.println(i);
 	}
 }
