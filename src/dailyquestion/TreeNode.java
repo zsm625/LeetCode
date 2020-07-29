9c8acade436904af84a90c2e19249056ebc79c34
zsm625 <zhoushimin_625@163.com>
2020-07-29 09:07:10 +0800

    树结构

---
+++ src/dailyquestion/TreeNode.java
@@ -0,0 +1,18 @@
+package dailyquestion;
+
+/**
+ * 树结构
+ * @author ZSM
+ *
+ * @time 2020年7月29日上午8:56:48
+ */
+public class TreeNode {
+
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int x) {
+		val = x;
+	}
+}
