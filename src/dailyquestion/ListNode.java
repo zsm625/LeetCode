3ec5f679c172692fe5baf7a11faa7e5a7425c265
zsm625 <zhoushimin_625@163.com>
2020-08-08 08:52:48 +0800

    单向链表

---
+++ src/dailyquestion/ListNode.java
@@ -0,0 +1,16 @@
+package dailyquestion;
+
+/**
+ * ListNode 定义单向链表
+ * @author ZSM
+ *
+ * @time 2020年8月8日上午8:07:03
+ */
+public class ListNode {
+	public int val;
+	public ListNode next;
+	public ListNode(int val) {
+		this.val = val;
+	}
+
+}
