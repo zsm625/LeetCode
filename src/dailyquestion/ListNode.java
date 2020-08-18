6649c796b3d9ba510aa7e93908de02b475e844aa
zsm625 <zhoushimin_625@163.com>
2020-08-18 10:29:37 +0800

    定义单向链表，添加含next指针的构造函数

--- src/dailyquestion/ListNode.java
+++ src/dailyquestion/ListNode.java
@@ -2,6 +2,7 @@ package dailyquestion;
 
 /**
  * ListNode 定义单向链表
+ * 
  * @author ZSM
  *
  * @time 2020年8月8日上午8:07:03
@@ -9,8 +10,13 @@ package dailyquestion;
 public class ListNode {
 	public int val;
 	public ListNode next;
+
 	public ListNode(int val) {
 		this.val = val;
 	}
 
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
 }
