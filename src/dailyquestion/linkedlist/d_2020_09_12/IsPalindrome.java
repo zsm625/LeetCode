92aea3d3aac98ee800c3d77f066bd4071eebe9c3
zsm625 <zhoushimin_625@163.com>
2020-09-12 12:33:16 +0800

    调整一下，将slow进行反转，再进行比较

--- src/dailyquestion/linkedlist/d_2020_09_12/IsPalindrome.java
+++ src/dailyquestion/linkedlist/d_2020_09_12/IsPalindrome.java
@@ -13,7 +13,7 @@ import dailyquestion.ListNode;
 public class IsPalindrome {
 	/**
 	 * 快慢指针到达中点，如果是偶数长度，那么fast不会为null，将slow往后移一位
-	 * 再将fast置为head，比较快慢指针的值
+	 * 再将fast置为head，并反转slow，比较快慢指针的值
 	 * @param head
 	 * @return
 	 */
@@ -29,6 +29,7 @@ public class IsPalindrome {
 		if(fast!=null) {
 			slow = slow.next;
 		}
+		slow = reverse(slow);
 		fast = head;
 		while(slow!=null) {
 			if(slow.val!=fast.val) {
@@ -40,4 +41,33 @@ public class IsPalindrome {
 		return true;
 	}
 
+	/**
+	 * 反转
+	 * @param node
+	 * @return
+	 */
+	public ListNode reverse(ListNode node) {
+		ListNode pre = null;
+		while(node!=null) {
+			ListNode next = node.next;
+			node.next = pre;
+			pre = node;
+			node = next;
+		}
+		return pre;
+	}
+	
+	public static void main(String[] args) {
+		IsPalindrome palindrome = new IsPalindrome();
+		ListNode head = new ListNode(1);
+		ListNode head2 = new ListNode(2);
+		ListNode head3 = new ListNode(2);
+		ListNode head4 = new ListNode(1);
+		head.next = head2;
+		head2.next = head3;
+		head3.next = head4;
+		boolean b = palindrome.isPalindrome(head);
+		System.out.println(b);
+		
+	}
 }
