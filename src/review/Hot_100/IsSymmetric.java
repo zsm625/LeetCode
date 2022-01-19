6f93af8f9756b710b06f9dea58d9a1d06a828f82
zsm625 <50209278+zsm625@users.noreply.github.com>
2022-01-19 13:46:23 +0800

    增加迭代方法

--- src/review/Hot_100/IsSymmetric.java
+++ src/review/Hot_100/IsSymmetric.java
@@ -15,7 +15,7 @@
  */
 class IsSymmetric {
     public boolean isSymmetric(TreeNode root) {
-        //用两个指针分别遍历
+        //用两个指针分别遍历// 递归
         return check(root,root);
     }
 
@@ -29,5 +29,36 @@ class IsSymmetric {
         int leftVal = left.val;
         int rightVal = right.val;
         return (leftVal==rightVal) && check(left.right,right.left) && check(left.left,right.right);
+    }
+    
+    // 迭代
+     private boolean IterationCheck(TreeNode left,TreeNode right){
+        if(left==null && right==null){
+            return true;
+        }
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(left);
+        deque.offer(right);
+        while(!deque.isEmpty()){
+             left = deque.poll();
+             right = deque.poll();
+            if(left==null && right == null){
+                continue;
+            }
+            if(left==null || right==null){
+                return false;
+            }
+
+            int val1 = left.val;
+            int val2= right.val;
+            if(val1!=val2){
+                return false;
+            }
+            deque.offer(left.left);
+            deque.offer(right.right);
+            deque.offer(left.right);
+            deque.offer(right.left);
+        }
+        return true;
     }
 }
