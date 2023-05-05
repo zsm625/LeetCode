924ccfea08934aac2725a2eb89437705486ac020
zsm625 <50209278+zsm625@users.noreply.github.com>
2023-05-05 13:41:04 +0800

    189. 轮转数组

--- src/zijie_plan/Rotate.java
+++ src/zijie_plan/Rotate.java
@@ -11,4 +11,32 @@ class Rotate {
        }
 
     }
+     public void rotate2(int[] nums, int k) {
+       // 把要轮转的数组看成一个环，每次移动步数，最后到达原点就不退出
+       int len = nums.length;
+       if(len < 0){
+           return;
+       }
+       int cnt = 0;
+       for(int i = 0;i<len;i++){
+           int temp = nums[i];
+           int n = i;
+           while(n<len){
+           int tmp = nums[(n+k) % len];
+           nums[(n+k) % len] = temp;
+           temp = tmp;
+           cnt++;
+           if((n+k) % len == i){
+               break;
+              }
+             n = (n+k) % len;
+          }
+          if(cnt == len){
+              break;
+          }
+       }
+       
+       
+
+    }
 }
