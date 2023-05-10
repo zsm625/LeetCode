28ddd7177d1426bebf78a0f9f322e1e09c268775
zsm625 <50209278+zsm625@users.noreply.github.com>
2023-05-10 10:35:37 +0800

    238. 除自身以外数组的乘积

--- src/zijie_plan/ProductExceptSelf.java
+++ src/zijie_plan/ProductExceptSelf.java
@@ -1,5 +1,5 @@
 class ProductExceptSelf {
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelf1(int[] nums) {
         int total = 1;
         int zeroCnt = 0;
         //暴力遍历 将总乘积得出，剔除为0的特殊例子 虽然代码low，但是自己的孩子不嫌弃 
@@ -25,4 +25,24 @@ class ProductExceptSelf {
         }
         return nums;
     }
+     public int[] productExceptSelf2(int[] nums) {
+        //换一种，题目要求不能使用除法 分别获取左右两边的乘积
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0]= 1;
+        right[n-1]=1;
+        int[] answer = new int[n];
+        for(int i=1;i<n;i++){
+            left[i] = nums[i-1]*left[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            right[i] = nums[i+1]*right[i+1];
+        }
+        for(int i=0;i<n;i++){
+            answer[i] = right[i]*left[i];
+        }
+        return answer;
+    }
+    
 }
