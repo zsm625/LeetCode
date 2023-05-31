e17253a5f8748be8c39e599190b63181a7c4d9e8
zsm625 <50209278+zsm625@users.noreply.github.com>
2023-05-31 10:19:58 +0800

    11. 盛最多水的容器

--- src/zijie_plan/MaxArea.java
+++ src/zijie_plan/MaxArea.java
@@ -18,4 +18,24 @@ class MaxArea {
         }
         return maxVolum;
     }
+    public int maxArea2(int[] height) {
+        // 暴力遍历 超长用例超时
+        int maxVolum = 0;
+        int hLen = height.length;
+        //双指针，左右指针分别置于左右终端，
+        //左右移动时，减少间隔，面积-1，
+        //长板移动，下一个面积不变或者减少，短板移动，下一个面积可能不变或者增大，因此每次移动短板
+        int left = 0,right = hLen-1;
+        while(left<right){
+            int minHei = Math.min(height[left],height[right]);
+            int volum = minHei*(right-left);
+            maxVolum = Math.max(volum,maxVolum);
+            if(height[left]<=height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxVolum;
+    }
 }
