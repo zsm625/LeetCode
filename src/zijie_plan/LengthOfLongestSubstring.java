f797bdcccc0c7512e5fcb1b8864747a3ef7989cc
zsm625 <50209278+zsm625@users.noreply.github.com>
2023-06-01 14:09:44 +0800

    3. 无重复字符的最长子串

--- src/zijie_plan/LengthOfLongestSubstring.java
+++ src/zijie_plan/LengthOfLongestSubstring.java
@@ -22,4 +22,21 @@ class LengthOfLongestSubstring {
         return ans;
 
     }
+    
+    //滑动窗口
+    public int lengthOfLongestSubstring2(String s) {
+        int start=0;
+        int len = s.length();
+        int ans = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<len;i++){
+            if(map.containsKey(s.charAt(i))){
+                start = Math.max(map.get(s.charAt(i))+1,start);
+            }
+            map.put(s.charAt(i),i);
+            ans = Math.max(i-start+1,ans);
+        }
+        return ans;
+
+    }
 }
