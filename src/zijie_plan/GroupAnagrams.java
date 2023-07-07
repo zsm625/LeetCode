1566058b34d6ea9b6dd0ddc3b8dabe1f7176e003
zsm625 <50209278+zsm625@users.noreply.github.com>
2023-07-07 10:57:47 +0800

    49. 字母异位词分组

--- src/zijie_plan/GroupAnagrams.java
+++ src/zijie_plan/GroupAnagrams.java
@@ -26,4 +26,45 @@ class GroupAnagrams {
         return res;
         
     }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        //方法二：将每个字符串中字符出现的个数与字符组成一个新的字符串作为key
+        List<List<String>> res = new ArrayList<>();
+        Map<String,List<String>> wordsMap = new HashMap<>();
+        for(String s:strs){
+            char[] chas = s.toCharArray();
+            String countStr = counterStr(chas);
+            if(wordsMap.containsKey(countStr)){
+                List<String> list = wordsMap.get(countStr);
+                list.add(s);
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                wordsMap.put(countStr,list);
+            }
+        }
+        if(!wordsMap.isEmpty()){
+            Set<String> keys =  wordsMap.keySet();
+            for(String key:keys){
+                res.add(wordsMap.get(key));
+            }
+        }
+        return res;
+        
+    }
+
+    public String counterStr(char[] chas){
+        char[] counter = new char[26];
+        for(int i=0;i<chas.length;i++){
+            counter[chas[i]-'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<counter.length;i++){
+            if(counter[i]>0){
+                sb.append((char)'a'+i);
+                sb.append(counter[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
