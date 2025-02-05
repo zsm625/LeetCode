86adc0df67a9669fdcbab2d1c810b45047caa481
zsm625 <50209278+zsm625@users.noreply.github.com>
2025-02-05 17:49:38 +0800

    盛最多水的容器

--- src/hot100/盛最多水的容器/MaxArea.java
+++ src/hot100/盛最多水的容器/MaxArea.java
@@ -1,35 +1,42 @@
 class MaxArea {
     public int maxArea(int[] height) {
-        
-        int length = height.length;
-
-        int l = 0;
-        int r = length - 1;
+        // 双指针,找大的的算面积
+        int left = 0;
+        int right = height.length-1;
         int maxArea = 0;
-        while (l < r) {
-
-            if (height[l] > height[r]) {
-                int temp_height = height[r];
-                int tempArea = (r - l) * temp_height;
-                if (maxArea < tempArea) {
-                    maxArea = tempArea;
+        while(left<height.length-1 && left < right){
+             int length= Math.min(height[left],height[right]);
+                int wide = right-left;
+                int area = length*wide;
+                maxArea = Math.max(area,maxArea);
+                if(height[left]<=height[right]){
+                    left++;
+                }else{
+                    right--;
                 }
-                do {
-                    r--;
-                } while(l < r && !(height[r] > temp_height));
-            } else {
-                int temp_height = height[l];
-                int tempArea = (r - l) * temp_height;
-                if (maxArea < tempArea) {
-                    maxArea = tempArea;
-                }
-                do {
-                    l++;
-                } while(l < r && !(height[l] > temp_height));
-            }
+            
         }
-
         return maxArea;
-    
+
+    }
+
+
+    private int maxArea1(int[] height) {
+        // 双指针,一个一个矩形遍历过去 离谱超时
+        int left = 0;
+        int maxArea = 0;
+        while(left<height.length-1){
+             int right = left+1;
+            while(right<height.length){
+                int length= Math.min(height[left],height[right]);
+                int wide = right-left;
+                int area = length*wide;
+                maxArea = Math.max(area,maxArea);
+                right++;
+            }
+            left++;
+        }
+        return maxArea;
+
     }
 }
